3c2def927426358f7c88181d717ab10ba3629911
malevi <malevi.ch@hotmail.com>
2014-12-01 21:47:33 +0000

    date of birth

--- WebSite6/PersonalInfo.aspx
+++ WebSite6/PersonalInfo.aspx
@@ -25,15 +25,23 @@
         </header>
         <div id="wrapper">
             <div class="wrap_title" id="rooms">
+                    <asp:SqlDataSource ID="SqlDataSource1" runat="server" 
+                        ConnectionString="<%$ ConnectionStrings:PersonalData_ConnectionString %>" 
+                        onselecting="SqlDataSource1_Selecting" 
+                        SelectCommand="SELECT * FROM [PersonalData]">
+                    </asp:SqlDataSource>
                 <h1>
                     Details</h1>
                
                 <div class="content_area">
                     <div class="content_wrap">
                         
+                        <asp:Label ID="lbl_Message" runat="server"></asp:Label>
+                        <br />
+                        
                         <asp:Label ID="Label1" runat="server" Text="What will be the year of study? *"></asp:Label>
                         <br />
-                        <asp:DropDownList ID="DropDownList1" runat="server" 
+                        <asp:DropDownList ID="dp_StudyYear" runat="server" 
                             onselectedindexchanged="DropDownList1_SelectedIndexChanged" Width="1000px">
                             <asp:ListItem Selected="True">Please Select...</asp:ListItem>
                             <asp:ListItem Value="Applicant">Applicant</asp:ListItem>
@@ -61,17 +69,26 @@
                         <asp:RadioButton ID="smokerNo" runat="server" Text="No" Width="100px" />
                         <asp:RadioButton ID="smokerYes" runat="server" Text="Yes" Width="100px" />
                         <br />
+                        <br />
                         <asp:Label ID="Label5" runat="server" 
                             Text="Please note that all our properties are non-smoking."></asp:Label>
                         <br />
                         <br />
                         <asp:Label ID="Label6" runat="server" Text="Date of birth *"></asp:Label>
                         <br />
+                        <asp:DropDownList ID="dp_Day" runat="server">
+                        </asp:DropDownList>
+&nbsp;
+                        <asp:DropDownList ID="dp_month" runat="server">
+                        </asp:DropDownList>
+&nbsp;
+                        <asp:DropDownList ID="dp_year" runat="server">
+                        </asp:DropDownList>
                         <br />
                         <br />
                         <asp:Label ID="Label7" runat="server" Text="Nationality *"></asp:Label>
                         <br />
-                        <asp:DropDownList ID="nationalityList" runat="server" 
+                        <asp:DropDownList ID="dp_nationalityList" runat="server" 
                             onselectedindexchanged="DropDownList2_SelectedIndexChanged" Width="1000px">
                             <asp:ListItem Selected="True">Please Select...</asp:ListItem>
                         </asp:DropDownList>
@@ -115,6 +132,9 @@
                             <asp:Label ID="Label14" runat="server" Text="Postcode *"></asp:Label>
                             <br />
                             <asp:TextBox ID="PostcodeTBox" runat="server" Width="1000px"></asp:TextBox>
+                            <br />
+                            <asp:Button ID="btn_next" runat="server" onclick="btn_next_Click" 
+                                style="height: 26px" Text="Next" />
                         </asp:Panel>
                         
                     </div>
--- WebSite6/PersonalInfo.aspx.cs
+++ WebSite6/PersonalInfo.aspx.cs
@@ -4,12 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class PersonalInfo : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        dp_Day.Items.Insert(0, new ListItem("dd"));
+        for(int i = 1; i<=31; i++)
+        {
+            string day;
+            if (i < 10)
+            {
+                day = "0" + i.ToString();
+            }
+            else
+            {
+                day = i.ToString();
+            }
+            dp_Day.Items.Insert(i, new ListItem(day));
+        }
 
+        dp_month.Items.Insert(0, new ListItem("mm"));
+        for (int i = 1; i <= 12; i++)
+        {
+            string month;
+            if (i < 10)
+            {
+                month = "0" + i.ToString();
+            }
+            else
+            {
+                month = i.ToString();
+            }
+            dp_month.Items.Insert(i, new ListItem(month.ToString()));
+        }
+
+        dp_year.Items.Insert(0, new ListItem("yyyy"));
+        for (int i = 1970; i <= 2014; i++)
+        {
+            dp_year.Items.Insert(i-1969, new ListItem(i.ToString()));
+        }
     }
     protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
     {
@@ -19,4 +55,154 @@ public partial class PersonalInfo : System.Web.UI.Page
     {
 
     }
+    protected void btn_next_Click(object sender, EventArgs e)
+    {
+    //   lbl_Message.Text = "";
+    //   //Connection with the Internal server
+    //   SqlConnection connect = new SqlConnection(ConfigurationManager.ConnectionStrings["Registration_ConnectionString"].ConnectionString);
+    //   connect.Open();
+    //   //Create the query in order to check if the email exist
+    //   string validateUser = "select count(*) from Users where email='" + txt_RegisterEmail.Text + "'";
+    //   SqlCommand Emailcommand = new SqlCommand(validateUser,connect);
+    //   int temp = Convert.ToInt32(Emailcommand.ExecuteScalar().ToString());
+    //   connect.Close();
+    //   //If it exist it check throw an exeption!
+    //   if (temp == 1)
+    //   {
+    //       lbl_Message.Text = "Email already exist!!";
+    //   }
+    //   else
+    //   {
+    //       string labelString = "";
+    //       Boolean register = true;
+
+    //       if (txt_Name.Text == "")
+    //       {
+    //           labelString += "Enter your First Name @";
+    //           register = false;
+    //       }
+
+
+    //       if (txt_Surname.Text == "")
+    //       {
+    //           labelString += "Enter your Last Name @";
+    //           register = false;
+    //       }
+
+    //       //Boolean validEmail = IsValidEmail(txt_RegisterEmail.Text);
+    //       //if (validEmail == false)
+    //       //{
+    //       //    labelString += "Enter a Valid Email @";
+    //       //    register = false;
+    //       //}
+
+    //       //string emailValidator = txt_RegisterEmail.Text;
+    //       //int sym = 0;
+    //       //char[] emailarray = emailValidator.ToCharArray();
+    //       //try
+    //       //{
+    //       //    if (char.IsLetter(emailarray[0]))
+    //       //    {
+    //       //        for (int i = 1; i < emailValidator.Length; i++)
+    //       //        {
+    //       //            if (emailValidator[i] == '@')
+    //       //            {
+    //       //                sym += 1;
+    //       //            }
+    //       //        }
+    //       //        sym += 1;
+    //       //    }
+    //       //}
+    //       //catch (Exception ex)
+    //       //{ 
+           
+    //       //}
+
+    //       if (txt_RegisterEmail.Text != txt_RegisterEmailConfirmation.Text)
+    //       {
+    //           txt_RegisterEmail.ForeColor = System.Drawing.Color.Red;
+    //           txt_RegisterEmailConfirmation.ForeColor = System.Drawing.Color.Red;
+    //           labelString += "The emails you entered are not maching @";
+    //           register = false;
+    //       }
+
+    //       //if (sym != 2)
+    //       //{
+    //       //    labelString += "Enter a Valid Email @";
+    //       //    register = false;
+    //       //}
+
+    //       if (txt_RegistrationPassword.Text != txt_RegisterPasswordConfirmation.Text)
+    //       {
+    //           labelString +=  "The passwords you entered are not maching @";
+    //           register = false;
+    //       }
+
+    //       string temp1 = txt_RegistrationPassword.Text;
+    //       int letter = 0;
+    //       int number = 0;
+    //       if ((temp1 == "") || (temp1.Length <= 6))
+    //       {
+    //           txt_RegistrationPassword.ForeColor = System.Drawing.Color.Red;
+    //           txt_RegisterPasswordConfirmation.ForeColor = System.Drawing.Color.Red;
+    //           labelString += "Your password needs to be at least 6 characters and at least one character and one number. @";
+    //           register = false;
+    //       }
+    //       else
+    //       {
+    //           for (int i = 0; i < temp1.Length; i++)
+    //           {
+    //               if (char.IsLetter(temp1[i]))
+    //               {
+    //                   letter = 1;
+    //               }
+    //               if (char.IsDigit(temp1[i]))
+    //               {
+    //                   number = 1;
+    //               }
+    //           }
+    //           if ((letter == 0) || (number == 0))
+    //           {
+    //               txt_RegistrationPassword.ForeColor = System.Drawing.Color.Red;
+    //               txt_RegisterPasswordConfirmation.ForeColor = System.Drawing.Color.Red;
+    //               labelString += "Your password needs to be at least 6 characters and at least one character and one number. @";
+    //               register = false;
+    //           }
+    //       }
+           
+    //       if (register == true)
+    //       {
+    //            try
+    //            {
+    //                connect.Open();
+    //                string newUser = "insert into Users (Email,FirstName,LastName,Password,Title) values(@email, @first, @last, @password, @title)";
+    //                SqlCommand newUserCommand = new SqlCommand(newUser, connect);
+    //                newUserCommand.Parameters.AddWithValue("@email", txt_RegisterEmail.Text);
+    //                newUserCommand.Parameters.AddWithValue("@first", txt_Name.Text);
+    //                newUserCommand.Parameters.AddWithValue("@last", txt_Surname.Text);
+    //                newUserCommand.Parameters.AddWithValue("@password", txt_RegistrationPassword.Text);
+    //                newUserCommand.Parameters.AddWithValue("@title", DropDownList1.SelectedItem.ToString());
+
+    //                newUserCommand.ExecuteNonQuery();
+    //                lbl_Message.Text = "Registration Complete";
+    //                Response.Redirect("homepage.aspx");
+    //                connect.Close();
+
+    //            }
+    //            catch (Exception ex)
+    //            {
+    //                lbl_Message.Text = "error:" + ex.ToString();
+    //            }
+    //       }
+    //       else
+    //       {
+    //           labelString = labelString.Replace("@", "<br />");
+    //           lbl_Message.Text = labelString;
+    //           lbl_Message.ForeColor = System.Drawing.Color.Red;
+    //       }
+
+    //    }
+    }
+
+   public string endl { get; set; }
 }
